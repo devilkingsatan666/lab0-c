9e1e449eafe98a48f116bf50fe880cc0e049abaa
Wu-Cheng Chiang <devilkingsatan666@gmail.com>
2024-03-02 21:21:51 +0800

    Implement q_remove_tail
    Check if head is NULL and empty. Check if sp is valid before copying
    value to it. After copying, make sure the copied string is null
    terminated.

--- queue.c
+++ queue.c
@@ -107,7 +107,19 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (head == NULL || list_empty(head))
+        return NULL;
+
+    element_t *popped = list_entry(head->prev, element_t, list);
+    list_del_init(head->prev);
+
+    if (sp != NULL) {
+        size_t len = bufsize - 1;
+        strncpy(sp, popped->value, len);
+        sp[len] = '\0';
+    }
+
+    return popped;
 }
 
 /* Return number of elements in queue */
