8ca1f5cdaf7eeedd62aa410376ebe4c6369a9fb2
Wu-Cheng Chiang <devilkingsatan666@gmail.com>
2024-03-02 21:03:39 +0800

    Implement API q_remove_head
    Check if head is null before operating on it. Check if sp is valid
    before copying popped item value to it. Make sure copied string is
    terminated with '\0'

--- queue.c
+++ queue.c
@@ -89,7 +89,19 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (head == NULL)
+        return NULL;
+
+    element_t *popped = list_entry(head->next, element_t, list);
+    list_del_init(head->next);
+
+    if (sp != NULL) {
+        size_t len = bufsize - 1;
+        strncpy(sp, popped->value, len);
+        sp[len] = '\0';
+    }
+
+    return popped;
 }
 
 /* Remove an element from tail of queue */
