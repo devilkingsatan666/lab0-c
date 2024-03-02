3278d3f202568bb26b8f1cc3a0e2368bcbaab939
Wu-Cheng Chiang <devilkingsatan666@gmail.com>
2024-03-02 22:48:00 +0800

    Implement q_delete_mid
    Check if head is NULL and empty before traversing through it. Use two
    iterators: slow and fast to go through the list. Fast runs at twice the
    speed slow does. If fast or its next hits the queue head, slow sits on
    the middle of the queue. Slow is then unlinked and uninitialized before
    being freed.

--- queue.c
+++ queue.c
@@ -141,6 +141,20 @@ int q_size(struct list_head *head)
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+
+    if (head == NULL || list_empty(head))
+        return false;
+
+    struct list_head *slow = head->next, *fast = head->next;
+
+    while (fast != head && fast->next != head) {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+
+    list_del(slow);
+    q_release_element(list_entry(slow, element_t, list));
+
     return true;
 }
 
